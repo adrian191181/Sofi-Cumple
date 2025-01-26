02834e83c770e3cff80b9b84f7ccb6d1d10e7b71
Adrian <gcangeladrian@gmail.com>
2025-01-26 16:28:20 -0600

    Frase

--- js/navbar.js
+++ js/navbar.js
@@ -13,3 +13,23 @@ window.addEventListener("scroll", () => {
 
   lastScrollTop = scrollTop;
 });
+
+const text = "Hoy quiero compartir esta alegría con todas las personas que hacen parte de mi vida, que me aman, me quieren y respetan. Tú eres una de ellas. Deseo que seas parte de este momento tan especial para mí.";
+const typingDiv = document.getElementById("frase");
+const startButton = document.getElementById("startTyping");
+
+let index = 0;
+
+function typeText() {
+  if (index < text.length) {
+    typingDiv.textContent += text[index];
+    index++;
+    setTimeout(typeText, 70); // Controla la velocidad (100 ms por carácter)
+  }
+}
+startButton.addEventListener("click", () => {
+  // Reiniciar animación si se presiona nuevamente
+  typingDiv.textContent = "";
+  index = 0;
+  typeText();
+});
